d18124f71aec9b58c8df34c9a5a7670a818bface
ffarrabal <ffarrabal@gmail.com>
2015-09-10 00:18:59 +0200

    sprint 2 new files

---
+++ src/js/fg-arv/libs/richmarkerPosition.js
@@ -0,0 +1,15 @@
+define(function() {
+
+  var RichMarkerPosition = {
+    'TOP_LEFT': 1,
+    'TOP': 2,
+    'TOP_RIGHT': 3,
+    'LEFT': 4,
+    'MIDDLE': 5,
+    'RIGHT': 6,
+    'BOTTOM_LEFT': 7,
+    'BOTTOM': 8,
+    'BOTTOM_RIGHT': 9
+  };
+  return RichMarkerPosition;
+});
